3a8dfdffa8a127efaf70c0d17da41ec1d3e062fa
nicholaskyar <nicholas.kyar@oit.edu>
2014-12-01 19:18:54 -0800

    Recreated files that were not committed previously. For some reason, some files never were comitted to the DB, and thus needed to be recreated.

---
+++ Lab5/Account/Account.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="Lab5.WebForm7" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ Lab5/Account/Logout.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(User.Identity.IsAuthenticated)
+            {
+                FormsAuthentication.SignOut();
+            }
+        }
+    }
+}
